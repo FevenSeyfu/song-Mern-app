cc96dcc7e342846e50f11c2e8ef4391c77735841
FevenSeyfu <fevenseyfu00@gmail.com>
2024-02-12 16:17:04 +0300

    Setup routes and Controller functions

---
+++ controllers/SongControllers.js
@@ -0,0 +1,16 @@
+import { Song } from "../models/SongModel";
+
+// create
+export const createSong = async (req,res) => {}
+
+// List songs
+export const listSongs = async (req,res) => {}
+
+// Update
+export const updateSong = async (req,res) => {}
+
+// Remove
+export const removeSong = async (req,res) => {}
+
+//overall statistics
+export const returnStatistics = async (req,res) => {}

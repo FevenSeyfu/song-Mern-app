9e26702d1f9362161ba9ecb3589399d242a19dea
FevenSeyfu <fevenseyfu00@gmail.com>
2024-02-12 23:43:22 +0300

    Add more stats in the controller

--- backend/controllers/SongControllers.js
+++ backend/controllers/SongControllers.js
@@ -6,7 +6,7 @@ export const createSong = async (req, res) => {
   try {
     if (!title || !artist || !album || !genre) {
       return response.status(400).json({
-        success : false,
+        success: false,
         message: "send all required fields:title,artist,album,genre ",
       });
     }
@@ -17,7 +17,13 @@ export const createSong = async (req, res) => {
       genre,
     };
     const song = await Song.create(newSong);
-    return res.status(201).json({success:true,message:'Song Successfully Added' ,result:song});
+    return res
+      .status(201)
+      .json({
+        success: true,
+        message: "Song Successfully Added",
+        result: song,
+      });
   } catch (error) {
     console.log(error.message);
     res.status(500).json({ success: false, message: error.message });
@@ -28,7 +34,7 @@ export const createSong = async (req, res) => {
 export const listSongs = async (req, res) => {
   try {
     const songs = await Song.find();
-    return res.status(200).json({success:true,result:songs});
+    return res.status(200).json({ success: true, result: songs });
   } catch (error) {
     console.log(error.message);
     res.status(500).json({ success: false, message: error.message });
@@ -41,13 +47,23 @@ export const updateSong = async (req, res) => {
   try {
     const { id } = req.params;
     if (!title || !artist || !album || !genre) {
-      return res.status(400).json({ success:false ,message: "send all required fields" });
+      return res
+        .status(400)
+        .json({ success: false, message: "send all required fields" });
     }
     const song = await Song.findByIdAndUpdate(id, req.body);
     if (!song) {
-      return res.status(404).json({ success:false, message: "Song not found" });
+      return res
+        .status(404)
+        .json({ success: false, message: "Song not found" });
     }
-    return res.status(200).json({ success:true ,message: "Song Updated successfully",result:song });
+    return res
+      .status(200)
+      .json({
+        success: true,
+        message: "Song Updated successfully",
+        result: song,
+      });
   } catch (error) {
     console.log(error.message);
     res.status(500).json({ success: false, message: error.message });
@@ -61,9 +77,17 @@ export const removeSong = async (req, res) => {
     const song = await Song.findByIdAndDelete(id);
 
     if (!song) {
-      return res.status(404).json({ success: false,message: "Song not found" });
+      return res
+        .status(404)
+        .json({ success: false, message: "Song not found" });
     }
-    return res.status(200).json({ success:true,message: "Song Deleted Successfully",result:song });
+    return res
+      .status(200)
+      .json({
+        success: true,
+        message: "Song Deleted Successfully",
+        result: song,
+      });
   } catch (error) {
     console.log(error.message);
     res.status(500).json({ success: false, message: error.message });
@@ -86,15 +110,23 @@ export const returnStatistics = async (req, res) => {
     const songsByAlbum = await Song.aggregate([
       { $group: { _id: "$album", count: { $sum: 1 } } },
     ]);
+    const albumsByArtist = await Song.aggregate([
+      { $group: { _id: "$artist", albums: { $addToSet: "$album" } } },
+    ]);
 
-    return res.status(200).send({
-      totalSongs,
-      totalArtists,
-      totalAlbums,
-      totalGenres,
-      songsByGenre,
-      songsByArtist,
-      songsByAlbum,
+    return res.status(200).json({
+      success : true,
+      message: 'Successfully fetched over all statistics',
+      result: {
+        totalSongs,
+        totalArtists,
+        totalAlbums,
+        totalGenres,
+        songsByGenre,
+        songsByArtist,
+        songsByAlbum,
+        albumsByArtist,
+      },
     });
   } catch (error) {
     console.log(error.message);

93bdf343536aa510a5e58f0e2432f30d3d006d94
FevenSeyfu <fevenseyfu00@gmail.com>
2024-02-12 19:17:41 +0300

    complete backend Controllers and Route

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import config from "./config/config";
+import config from "./config/config.js";
+import SongRoutes from './routes/SongRoutes.js'
 const app = express();
 
 app.use(express.json());
@@ -16,14 +17,13 @@ app.use(
   })
 );
 app.use(express.json());
-app.use(cors(corsOptions));
 
 mongoose
   .connect(config.mongodbURI)
   .then(() => {
     const PORT =config.port;
     app.listen(PORT, () => {
-      console.log(`Server is running on PORT ${PORT}`);
+      console.log(`Server is running on Port ${PORT}`);
     });
   })
   .catch((err) => {
@@ -33,3 +33,5 @@ mongoose
 app.get("/", (req, res) => {
   res.status(201).json({ message: "Server is  up and running" });
 });
+
+app.use('/songs',SongRoutes);

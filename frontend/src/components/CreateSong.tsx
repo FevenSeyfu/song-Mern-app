0dc512d59aba4beee98a16fab7e08439718f639a
FevenSeyfu <fevenseyfu00@gmail.com>
2024-02-26 21:05:53 +0300

    Complete style of  create song modal

--- frontend/src/components/CreateSong.tsx
+++ frontend/src/components/CreateSong.tsx
@@ -1,56 +1,94 @@
-import React,{useState} from 'react'
-import { useDispatch,useSelector } from 'react-redux';
-import { createSong } from '../features/Song/SongSlice';
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { createSong } from "../features/Song/SongSlice";
 import { RootState } from "../app/store";
+import { IoClose } from "react-icons/io5";
+import {
+  CloseButton,
+  StyledForm,
+  StyledH1,
+  StyledLabel,
+  StyledButton,
+  StyledInput,
+} from "./FormStyle";
 
 interface CreateSongProps {
   onClose: () => void;
 }
-const CreateSong: React.FC<CreateSongProps> = ({onClose}) => {
+const CreateSong: React.FC<CreateSongProps> = ({ onClose }) => {
   const dispatch = useDispatch();
-  
-  const [title, setTitle] = useState('');
-  const [artist, setArtist] = useState('');
-  const [album, setAlbum] = useState('');
-  const [genre, setGenre] = useState('');
 
-  const handleSubmit = (e: React.FormEvent) =>{
+  const [title, setTitle] = useState("");
+  const [artist, setArtist] = useState("");
+  const [album, setAlbum] = useState("");
+  const [genre, setGenre] = useState("");
+
+  const handleSubmit = async(e: React.FormEvent) => {
     e.preventDefault();
-    const song = { title, artist, album , genre}
-    dispatch(createSong(song));
-    setTitle('');
-    setArtist('');
-    setAlbum('');
-    setGenre('');
+    const song = { title, artist, album, genre };
+    await dispatch(createSong(song));
+    setTitle("");
+    setArtist("");
+    setAlbum("");
+    setGenre("");
     onClose();
-  }
-  const {songs,isLoading,error} = useSelector((state: RootState) => state.songs);
+  };
+  const { songs, isLoading, error } = useSelector(
+    (state: RootState) => state.songs
+  );
   return (
-    <form onSubmit={handleSubmit}>
-      <h1>Create Song</h1>
+    <>
+      <CloseButton onClick={() => onClose()}>
+        <IoClose size={30} style={{ textAlign: "right" }} />
+      </CloseButton>
+      <StyledForm onSubmit={handleSubmit}>
+        <StyledH1>Create Song</StyledH1>
 
-      <label>
-        Title:
-        <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} required />
-      </label>
+        <StyledLabel>
+          Title:
+          <StyledInput
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            required
+          />
+        </StyledLabel>
 
-      <label>
-        Artist:
-        <input type="text" value={artist} onChange={(e) => setArtist(e.target.value)} required />
-      </label>
+        <StyledLabel>
+          Artist:
+          <StyledInput
+            type="text"
+            value={artist}
+            onChange={(e) => setArtist(e.target.value)}
+            required
+          />
+        </StyledLabel>
 
-      <label>
-        Album:
-        <input type="text" value={album} onChange={(e) => setAlbum(e.target.value)} required />
-      </label>
-      <label>
-        Genre:
-        <input type="text" value={genre} onChange={(e) => setGenre(e.target.value)} required />
-      </label>
+        <StyledLabel>
+          Album:
+          <StyledInput
+            type="text"
+            value={album}
+            onChange={(e) => setAlbum(e.target.value)}
+            required
+          />
+        </StyledLabel>
+        <StyledLabel>
+          Genre:
+          <StyledInput
+            type="text"
+            value={genre}
+            onChange={(e) => setGenre(e.target.value)}
+            required
+          />
+        </StyledLabel>
 
-      <button type="submit" disabled={isLoading}>{isLoading? 'Loading' : 'Create Song '}</button>
-    </form>
-  )
-}
+        <StyledButton type="submit" disabled={isLoading}>
+          {isLoading ? "Loading" : "Create Song "}
+        </StyledButton>
+      </StyledForm>
+    </>
+  );
+};
 
-export default CreateSong
+export default CreateSong;
